d127841cf361b14519f41242ad42e14aa0e0a168
Gints Kristaps Perkons <gintskristaps13@gmail.com>
2024-09-05 19:57:34 +0300

    basic react component

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,18 @@
+// main.js
 
-console.log('Hello from main.js');
+// Define a simple React component
+class MyComponent extends React.Component {
+    componentDidMount() {
+        console.log('MyComponent has been mounted!');
+    }
 
+    render() {
+        return React.createElement('div', null, 'Hello, React!');
+    }
+}
+
+// Render the React component into the root element
+ReactDOM.render(
+    React.createElement(MyComponent),
+    document.getElementById('root')
+);
